3e175b49abff64a7ef160bfa39546b32797bb2ae
Jun-KY <101626000+Jun-KY@users.noreply.github.com>
2022-03-15 16:43:04 +0900

    Add files via upload
    Lec 01

---
+++ src/Introduction.java
@@ -0,0 +1,12 @@
+
+public class Introduction {
+
+	public static void main(String[] args) {
+		System.out.println("안녕하세요?");
+		System.out.print("저는 자바를 처음 공부합니다.\n");
+		System.out.printf("%s", "저의 나이는 20살 입니다.");
+		System.out.println();
+		System.out.println("안녕하세요?\n저는 자바를 처음 공부합니다.\n저의 나이는 20살입니다.");
+	}
+
+}
---
+++ src/Test_Java.java
@@ -0,0 +1,10 @@
+
+public class Test_Java {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.printf("%s", "Hello");
+		System.out.println();
+	}
+
+}
